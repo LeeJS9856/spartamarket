f50ed6ce412d7554b527fa546e803fcd98fd6f70
LeeJS9856 <wnstj1057@naver.com>
2024-04-13 22:54:09 +0900

    로그인 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 from django.views.decorators.http import require_POST, require_http_methods
 
-@require_http_methods(["GET", "POST"])
+@require_http_methods(['GET', 'POST'])
 def login(request):
-    if request.method == 'POST':
-        pass
+    if request.method == "POST":
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            next_path = request.GET.get('next') or 'products:home'
+            return redirect(next_path)
     else:
         form = AuthenticationForm()
-        context = {
+    context = {
             'form': form,
-        }
+    }
     return render(request, 'accounts/login.html', context)
 
 @require_POST
