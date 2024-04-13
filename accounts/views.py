0a2074f0a58c75e1a4b9a1499bb723df2945af1c
LeeJS9856 <wnstj1057@naver.com>
2024-04-13 21:53:19 +0900

    login페이지 구동 확인

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST, require_http_methods
 
+@require_http_methods(["GET", "POST"])
+def login(request):
+    if request.method == 'POST':
+        pass
+    else:
+        form = AuthenticationForm()
+        context = {
+            'form': form,
+        }
+    return render(request, 'accounts/login.html', context)
+
+@require_POST
+def logout(request):
+    pass
 # Create your views here.
+
+def signup(request):
+    return render(request, 'accounts/signup.html')
