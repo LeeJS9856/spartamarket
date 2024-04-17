d238e97ff667abccd13a3b4e31e51b74639239ff
LeeJS9856 <wnstj1057@naver.com>
2024-04-17 19:02:00 +0900

    article.html css 구현

--- products/views.py
+++ products/views.py
@@ -1,72 +1,100 @@
+from datetime import datetime, timedelta
+from .forms import ArticleForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods, require_POST
-from.models import Article
-from .forms import ArticleForm
+from .models import Article
 
-def home(request): 
+
+def time_difference_in_words(time_str):
+    input_time = datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S.%f%z")
+    now = datetime.now(input_time.tzinfo)
+    time_diff = now - input_time
+    minutes_diff = time_diff.total_seconds() / 60
+
+    if minutes_diff < 10:
+        return '방금'
+    elif minutes_diff < 30:
+        return '10분 전'
+    elif minutes_diff < 60:
+        return '30분 전'
+    elif time_diff.days == 0:
+        return f'{int(minutes_diff / 60)}시간 전'
+    else:
+        return f'{time_diff.days}일 전'
+
+
+def home(request):
     articles = Article.objects.all()
-    context = {"articles" : articles}
+    context = {"articles": articles}
     return render(request, 'products/home.html', context)
 
-def article(request, id) :
+
+def article(request, id):
     article = get_object_or_404(Article, id=id)
-    context = {"article" : article}
+    article_time = time_difference_in_words(str(article.created_at))
+    context = {
+        "article": article,
+        "article_time": article_time,
+    }
     return render(request, 'products/article.html', context)
 
+
 @login_required
-def create(request) :
-    if request.method == "POST" :
+def create(request):
+    if request.method == "POST":
         form = ArticleForm(request.POST, request.FILES)
-        if form.is_valid() :
+        if form.is_valid():
             article = form.save(commit=False)
             article.author = request.user
-            article.save() 
+            article.save()
             return redirect("products:article", article.id)
-    else :
+    else:
         form = ArticleForm()
-    context = {"form" : form}
+    context = {"form": form}
     return render(request, 'products/create.html', context)
+
 
 @login_required
 @require_http_methods(["GET", "POST"])
-def edit(request, id) :
+def edit(request, id):
     article = get_object_or_404(Article, id=id)
-    if request.method == "POST" :
+    if request.method == "POST":
         form = ArticleForm(request.POST, request.FILES, instance=article)
-        if form.is_valid() :
+        if form.is_valid():
             article = form.save(commit=False)
             article.author = request.user
-            article.save() 
+            article.save()
             return redirect("products:article", article.id)
-    else :
+    else:
         form = ArticleForm(instance=article)
     context = {
-        "form" : form,
-        "article" : article,
-        }
+        "form": form,
+        "article": article,
+    }
     return render(request, 'products/edit.html', context)
 
+
 @require_POST
-def delete(request, id) :
+def delete(request, id):
     if request.user.is_authenticated:
         article = get_object_or_404(Article, id=id)
-        if request.method == "POST" :
+        if request.method == "POST":
             article.image.delete()
             article.delete()
             return redirect("products:home")
-        context = {"article" : article}
+        context = {"article": article}
         return render(request, 'products/delete.html', context)
-    
+
 
 @require_POST
-def like(request, id) :
+def like(request, id):
     if request.user.is_authenticated:
         article = get_object_or_404(Article, id=id)
-        if article.like_users.filter(id=request.user.id).exists() :
+        if article.like_users.filter(id=request.user.id).exists():
             article.like_users.remove(request.user)
-        else :
+        else:
             article.like_users.add(request.user)
         return redirect("products:article", article.id)
     return redirect("accounts:login")
