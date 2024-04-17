4eae99bd754948eb3b3fe032e99389018a581cb3
LeeJS9856 <wnstj1057@naver.com>
2024-04-17 11:28:47 +0900

    기존주소에서 home으로 이동하도록 변경

--- spartamarket/urls.py
+++ spartamarket/urls.py
@@ -19,9 +19,11 @@ from django.urls import path
 from django.urls.conf import include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home),
     path('accounts/', include('accounts.urls')),
     path('products/', include('products.urls')),
 ]
